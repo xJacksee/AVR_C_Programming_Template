737536c543d31b7981412605c8373460e6b8507e
Szymon Bartosik <szymon.bartosik75@gmail.com>
2024-09-01 18:13:46 +0200

    initial files

---
+++ makefile
@@ -0,0 +1,17 @@
+MCU = atmega328p
+DUDE_MCU = m328p
+PROG = stk500v1
+NAME = main
+PORT = /dev/ttyACM0
+
+upload: hex
+	avrdude -c $(PROG) -p $(DUDE_MCUMCU) -P $(PORT) -U flash:w:"./bin/$(NAME).hex":i
+
+hex: compile
+	avr-objcopy ./bin/$(SOURCE).elf -O ihex ./bin/main.hex
+
+compile:
+	avr-gcc ./src/$(SOURCE).c -o ./bin/$(SOURCE).elf -mmcu=$(MCU) -O1
+
+clean:
+	rm ./bin/*.elf && rm ./bin/*.hex
---
+++ src/main.c
@@ -0,0 +1,41 @@
+/*
+*************************
+* Project Name          *
+* Rev 1.0               *
+* Date:                 *
+* Author:               *
+*************************
+
+LICENCE:
+
+Description:
+
+*/
+
+#define F_CPU 1000000UL // Define clock frequency in Hz for delay.h library to work correctly
+
+//Include standard AVR libraries
+#include <avr/io.h>
+#include <util/delay.h>
+
+//Include external libraries
+
+//Function prototypes
+
+//Interrupts
+
+//Global variables
+
+//Main
+int main() {
+    //Initial setup
+
+    //Main loop
+    while(1) {
+
+    }
+
+    return 0;
+}
+
+//Functions
